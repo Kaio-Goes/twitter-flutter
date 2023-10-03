39e6839d887bfb105bbee773f10657a814087bf0
@kaio.goes <kaio.goes@spezi.com.br>
2023-10-03 14:08:17 -0300

    Feature config constants

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteContants {
+  static const String databeseId = '651c447262c1a673290d';
+  static const String projectId = '651a258f8d5ece8858a4';
+  static const String endPoint = 'http://localhost:80/v1';
+}
